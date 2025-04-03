d98f649b8e6ec7a121094f1bc7af55afceb3816d
Nara Bai <baianara207@gmail.com>
2025-04-03 09:46:31 +0300

    pr6_1

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -14,29 +14,29 @@ export default function CatalogPage(){
     const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
 
     return(
-            <section className={s.catalog}>
-                <div className="container">
-                    <div className={s.catalog_inner}>
-                        <h2 className={s.title}>Каталог товаров</h2>
-                        <Search handleChange={handleChange}/>
-                        <div className={s.nav}>
-                            <p className={s.text}>Категории:</p>
-                            <BlueBtn text="Все товары"/>
-                            <CatalogWhiteBtn text="Шины/колеса"/>
-                            <CatalogWhiteBtn text="Масла"/>
-                            <CatalogWhiteBtn text="Ароматизаторы"/>
-                        </div>
-                        <div className={s.cards}>
-                            {
-                                filteredProducts.length
-                                ?
-                                filteredProducts.map(product=><Card {...product}/>)
-                                :
-                                <p>По запросу "{search}" ничего не найдено</p>
-                            }
-                        </div>
+        <section className={s.catalog}>
+            <div className="container">
+                <div className={s.catalog_inner}>
+                    <h2 className={s.title}>Каталог товаров</h2>
+                    <Search handleChange={handleChange}/>
+                    <div className={s.nav}>
+                        <p className={s.text}>Категории:</p>
+                        <BlueBtn text="Все товары"/>
+                        <CatalogWhiteBtn text="Шины/колеса"/>
+                        <CatalogWhiteBtn text="Масла"/>
+                        <CatalogWhiteBtn text="Ароматизаторы"/>
+                    </div>
+                    <div className={s.cards}>
+                        {
+                            filteredProducts.length
+                            ?
+                            filteredProducts.map(product=><Card {...product}/>)
+                            :
+                            <p>По запросу "{search}" ничего не найдено</p>
+                        }
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
     )
 }
