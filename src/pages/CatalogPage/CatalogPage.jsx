d52ddd18580b4cb198b579c159b1704dac4d287a
Nara Bai <baianara207@gmail.com>
2025-04-03 09:28:21 +0300

    pr6

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -3,14 +3,22 @@ import BlueBtn from "../../components/BlueBtn/BlueBtn"
 import CatalogWhiteBtn from '../CatalogPage/CatalogWhiteBtn/CatalogWhiteBtn'
 import s from './CatalogPage.module.css'
 import { products } from "../../products"
+import { useState } from "react"
+import Search from "../../components/Search/Search"
 
 export default function CatalogPage(){
+    const [search, setSearch] = useState('');
+    function handleChange(e){
+        setSearch(e.target.value);
+    }
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+
     return(
-        <>
             <section className={s.catalog}>
                 <div className="container">
                     <div className={s.catalog_inner}>
                         <h2 className={s.title}>Каталог товаров</h2>
+                        <Search handleChange={handleChange}/>
                         <div className={s.nav}>
                             <p className={s.text}>Категории:</p>
                             <BlueBtn text="Все товары"/>
@@ -20,12 +28,15 @@ export default function CatalogPage(){
                         </div>
                         <div className={s.cards}>
                             {
-                                products.map(product=><Card {...product}/>)
+                                filteredProducts.length
+                                ?
+                                filteredProducts.map(product=><Card {...product}/>)
+                                :
+                                <p>По запросу "{search}" ничего не найдено</p>
                             }
                         </div>
                     </div>
                 </div>
             </section>
-        </>
     )
 }
