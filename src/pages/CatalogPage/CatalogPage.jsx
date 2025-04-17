3075a2c3a8f32c7ea0b1cb248c22a6f046852c60
Nara Bai <baianara207@gmail.com>
2025-04-17 09:25:43 +0300

    pr8

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -8,10 +8,27 @@ import Search from "../../components/Search/Search"
 
 export default function CatalogPage(){
     const [search, setSearch] = useState('');
+    const [sorting, setSorting] = useState(0);
+    const [category, setCategory] = useState('all');
+
+    function sort(e) {
+        setSorting(e.target.value);
+    }
+    function sortProducts(sorting, products) {
+        switch(sorting) {
+            case 'asc' : return [...products].sort((a,b)=>a.price-b.price);
+            case 'desc' : return [...products].sort((a,b)=>b.price-a.price);
+            default: return products;
+        }
+    }
     function handleChange(e){
         setSearch(e.target.value);
     }
-    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase())
+        &&
+        (product.categoryId == category || category == 'all')
+        );
+    const sortedAndFilteredProducts = sortProducts(sorting, filteredProducts);
 
     return(
         <section className={s.catalog}>
@@ -19,18 +36,24 @@ export default function CatalogPage(){
                 <div className={s.catalog_inner}>
                     <h2 className={s.title}>Каталог товаров</h2>
                     <Search handleChange={handleChange}/>
+                    <select value={sorting} onChange={sort} className={s.sortPrice}>
+                        <option value="0">Сортировать</option>
+                        <option value="asc">По возрастанию цены</option>
+                        <option value="desc">По убыванию цены</option>
+                    </select>
                     <div className={s.nav}>
                         <p className={s.text}>Категории:</p>
-                        <BlueBtn text="Все товары"/>
-                        <CatalogWhiteBtn text="Шины/колеса"/>
-                        <CatalogWhiteBtn text="Масла"/>
-                        <CatalogWhiteBtn text="Ароматизаторы"/>
+                        <button onClick={()=>setCategory('all')}><BlueBtn text="Все товары"/></button>
+                        <button onClick={()=>setCategory(0)}><CatalogWhiteBtn text="Шины/колеса" /></button>
+                        <button onClick={()=>setCategory(1)}><CatalogWhiteBtn text="Масла" /></button>
+                        <button onClick={()=>setCategory(2)}><CatalogWhiteBtn text="Ароматизаторы" /></button>
+                        <button onClick={()=>setCategory(3)}><CatalogWhiteBtn text="Автотовары" /></button>
                     </div>
                     <div className={s.cards}>
                         {
-                            filteredProducts.length
+                            sortedAndFilteredProducts.length
                             ?
-                            filteredProducts.map(product=><Card {...product}/>)
+                            sortedAndFilteredProducts.map(product=><Card {...product}/>)
                             :
                             <p>По запросу "{search}" ничего не найдено</p>
                         }
